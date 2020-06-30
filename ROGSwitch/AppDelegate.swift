93c93f66026c48b81a6bd84b7dca1250952fc5d3
black.dragon74 <nickk.2974@gmail.com>
2020-06-30 15:16:39 +0530

    Save app state across restarts and consistent LUX levels
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- ROGSwitch/AppDelegate.swift
+++ ROGSwitch/AppDelegate.swift
@@ -8,33 +8,113 @@
 
 import Cocoa
 
+enum ROGSwitchColors: Int, Codable {
+    case red = 1
+    case green
+    case magenta
+    case gold
+    case rainbow
+    case colorCycle
+    case breathing
+}
+
+struct ROGSwitchState: Codable {
+    var isIlluminated: Bool
+    var luxLevel: Int
+    var currentColor: ROGSwitchColors
+}
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     lazy var statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
     
-    private var kbdLightOn: Bool = true
-    private var brightnessLevel: Int = 3
+    private var appState = ROGSwitchState(isIlluminated: true, luxLevel: 3, currentColor: .red)
     
+    private let DEFAULT_KEY = "ROGSwitchState"
     private let rogAuraCore = "/usr/local/bin/macRogAuraCore"
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         
         if let button = statusItem.button {
-            button.toolTip = "Turn keyobard lights on or off"
+            button.toolTip = "Manage your ROG AURA keyboard"
             button.image = NSImage(named: "rog_insignia")
         }
         
         buildMenu()
         
-        // Turn on the keyboard lights on start
-        runShellCommand(rogAuraCore, args: ["brightness", "\(brightnessLevel)"])
+        // Read app's laste state from the DB
+        readValuesFromDB()
         
+        // Now restore the app state based on the DB data
+        restoreAppState()
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        saveValuesToDB() // An extra bit of safety measure
+    }
+    
+    fileprivate func readValuesFromDB() {
+        if let dbData = UserDefaults.standard.value(forKey: DEFAULT_KEY) as? Data {
+            // Now we need to decode the data
+            let decoder = JSONDecoder()
+            do {
+                let parsed = try decoder.decode(ROGSwitchState.self, from: dbData)
+                appState = parsed
+            } catch let ex {
+                print(ex.localizedDescription)
+            }
+        }
+    }
+    
+    fileprivate func saveValuesToDB() {
+        let encoder = JSONEncoder()
+        do {
+            let encMsg = try encoder.encode(appState)
+            UserDefaults.standard.removeObject(forKey: DEFAULT_KEY)
+            UserDefaults.standard.setValue(encMsg, forKey: DEFAULT_KEY)
+            UserDefaults.standard.synchronize()
+        } catch let ex {
+            print(ex.localizedDescription)
+        }
+    }
+    
+    fileprivate func restoreAppState() {
+        // App State restoration will take place in 3 steps
+        // - Restore the illuminated state
+        // - Restore the color state
+        // - Redtore the lux state
+        
+        // As every function call changes the appState, we copy the state before any calls
+        let copyState = appState
+        
+        if copyState.isIlluminated {
+            // Restore the color, setting a color automatically turns on the lights
+            switch copyState.currentColor {
+            case .red:
+                handleRed()
+            case .green:
+                handleGreen()
+            case .magenta:
+                handleMagenta()
+            case .gold:
+                handleGold()
+            case .rainbow:
+                handleRainbow()
+            case .breathing:
+                handleBreathingMode()
+            case .colorCycle:
+                handleColorCycle()
+                // No default state as this is an exhaustive switch case
+            }
+            
+            // Last step, restore the lux state
+            runShellCommand(rogAuraCore, args: ["brightness", "\(copyState.luxLevel)"])
+            appState.luxLevel = copyState.luxLevel
+        } else {
+            // Lights are off, do that
+            handleKbdOff()
+        }
     }
     
     fileprivate func buildMenu() {
@@ -65,87 +145,98 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     @objc fileprivate func handleKbdOn() {
-        if (kbdLightOn) {
+        if appState.isIlluminated {
             return
         }
         
         // Otherwise, turn on the keyboard lights full brightness
         runShellCommand(rogAuraCore, args: ["on"])
-        brightnessLevel = 3
-        kbdLightOn = true
+        appState.luxLevel = 3
+        appState.isIlluminated = true
+        appState.currentColor = .red
+        saveValuesToDB()
     }
     
     @objc fileprivate func handleKbdOff() {
-        if (!kbdLightOn) {
+        if !appState.isIlluminated {
             return
         }
         
         // Otherwise, turn off kbd lights
         runShellCommand(rogAuraCore, args: ["off"])
-        brightnessLevel = 0
-        kbdLightOn = false
+        appState.luxLevel = 0
+        appState.isIlluminated = false
+        saveValuesToDB()
     }
     
     @objc fileprivate func handleBrightnessUp() {
-        if (brightnessLevel >= 3) {
+        if appState.luxLevel >= 3 {
             return
         }
         
-        brightnessLevel += 1
-        runShellCommand(rogAuraCore, args: ["brightness", "\(brightnessLevel)"])
-        kbdLightOn = true
+        appState.luxLevel += 1
+        runShellCommand(rogAuraCore, args: ["brightness", "\(appState.luxLevel)"])
+        appState.isIlluminated = true
+        saveValuesToDB()
     }
     
     @objc fileprivate func handleBrightnessDown() {
-        if (brightnessLevel <= 0) {
+        if appState.luxLevel <= 0 {
             return
         }
         
-        brightnessLevel -= 1
-        runShellCommand(rogAuraCore, args: ["brightness", "\(brightnessLevel)"])
-        kbdLightOn = brightnessLevel != 0
+        appState.luxLevel -= 1
+        runShellCommand(rogAuraCore, args: ["brightness", "\(appState.luxLevel)"])
+        appState.isIlluminated = appState.luxLevel != 0
+        saveValuesToDB()
     }
     
     @objc fileprivate func handleRed() {
         runShellCommand(rogAuraCore, args: ["red"])
-        brightnessLevel = 3
-        kbdLightOn = true
+        appState.isIlluminated = true
+        appState.currentColor = .red
+        saveValuesToDB()
     }
     
     @objc fileprivate func handleGreen() {
         runShellCommand(rogAuraCore, args: ["green"])
-        brightnessLevel = 3
-        kbdLightOn = true
+        appState.isIlluminated = true
+        appState.currentColor = .green
+        saveValuesToDB()
     }
     
     @objc fileprivate func handleMagenta() {
         runShellCommand(rogAuraCore, args: ["magenta"])
-        brightnessLevel = 3
-        kbdLightOn = true
+        appState.isIlluminated = true
+        appState.currentColor = .magenta
+        saveValuesToDB()
     }
     
     @objc fileprivate func handleGold() {
         runShellCommand(rogAuraCore, args: ["gold"])
-        brightnessLevel = 3
-        kbdLightOn = true
+        appState.isIlluminated = true
+        appState.currentColor = .gold
+        saveValuesToDB()
     }
     
     @objc fileprivate func handleRainbow() {
         runShellCommand(rogAuraCore, args: ["rainbow"])
-        brightnessLevel = 3
-        kbdLightOn = true
+        appState.isIlluminated = true
+        appState.currentColor = .rainbow
+        saveValuesToDB()
     }
     
     @objc fileprivate func handleColorCycle() {
         runShellCommand(rogAuraCore, args: ["single_colorcycle", "1"])
-        brightnessLevel = 3
-        kbdLightOn = true
+        appState.isIlluminated = true
+        appState.currentColor = .colorCycle
+        saveValuesToDB()
     }
     
     @objc fileprivate func handleBreathingMode() {
         runShellCommand(rogAuraCore, args: ["multi_breathing", "ff0000", "00ff00", "0000ff", "00ffff", "2"])
-        brightnessLevel = 3
-        kbdLightOn = true
+        appState.isIlluminated = true
+        appState.currentColor = .breathing
+        saveValuesToDB()
     }
 }
-
